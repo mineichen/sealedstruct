1a054ec2a4973952dcd2fa4aa92bd89c08250f74
Markus Ineichen <markus.ineichen@lipautomation.com>
2022-10-03 11:56:59 +0200

    Add tests for SealSimple

---
+++ sealedstruct/tests/simple.rs
@@ -0,0 +1,19 @@
+#[derive(PartialEq, Default, Debug, sealedstruct::SealSimple, sealedstruct::Validator)]
+pub struct SimpleRaw {
+    pub inner: i8,
+}
+
+#[test]
+fn sealed_numbers_simple() {
+    let raw = SimpleRaw { inner: 0 };
+    let _sealed: Simple = raw.try_into().unwrap();
+}
+
+#[test]
+fn sealed_numbers_tuple() {
+    #[derive(PartialEq, Default, Debug, sealedstruct::SealSimple, sealedstruct::Validator)]
+    pub struct SimpleTupleRaw(i8);
+    sealedstruct::Result::<()>::from(SimpleTupleResult(Result::Ok(1))).unwrap();
+    let raw = SimpleTupleRaw(0);
+    let _sealed: SimpleTuple = raw.try_into().unwrap();
+}
